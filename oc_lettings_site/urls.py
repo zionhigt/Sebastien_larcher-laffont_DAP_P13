5c933bdd4d27f70c558c7157c8bfa4561d50e2fb
zionhigt <larcher501@gmail.com>
2022-06-13 11:08:31 +0200

    Finalize refactor models layer

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from lettings.views import lettings_index, letting
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
+    path('lettings/', lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', letting, name='letting'),
     path('profiles/', views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
