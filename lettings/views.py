d2ba9e232a29d0327e48b93c59d2f038e8c44f80
zionhigt <larcher501@gmail.com>
2022-06-13 11:19:05 +0200

    Set template path for render function

--- lettings/views.py
+++ lettings/views.py
@@ -11,7 +11,7 @@ def lettings_index(request):
     """
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 def letting(request, letting_id):
@@ -40,4 +40,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
