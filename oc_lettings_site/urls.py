0ac01e7818b355d45a2c5ab94b72ecad253ea21c
zionhigt <larcher501@gmail.com>
2022-06-18 10:29:24 +0200

    Settings route callback for new profiles django app views

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 
 from lettings.urls import lettings_extra_patterns
 from . import views
+from profiles.views import profiles_index, profile
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include(lettings_extra_patterns)),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
