191d2e71fd910613478168ab3cee7b89991b800a
zionhigt <larcher501@gmail.com>
2022-06-18 10:27:22 +0200

    Move profiles django app  templates files

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,7 @@ def profiles_index(request):
     """
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 
 def profile(request, username):
@@ -22,4 +22,4 @@ def profile(request, username):
     """
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
