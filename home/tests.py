9d1855da902bbae6fc6f0bf9f73042b08b702f4e
zionhigt <larcher501@gmail.com>
2022-07-02 09:24:28 +0200

    home/test.py pep8

--- home/tests.py
+++ home/tests.py
@@ -1,15 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class IndexTestCase(TestCase):
     def setUp(self):
         super().setUp()
-    
+
     def test_site_index_page(self):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
         self.assertContains(response, '<h1>Welcome to Holiday Homes</h1>')
-
-
-    
