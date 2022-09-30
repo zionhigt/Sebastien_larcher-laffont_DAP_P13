0afa66ee34cd1e56ab8b6fcaa1e4bde38b35485e
zionhigt <larcher501@gmail.com>
2022-09-30 14:26:42 +0200

    Enable Debug

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
